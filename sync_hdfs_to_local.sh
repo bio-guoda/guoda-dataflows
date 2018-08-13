6efeac9705db2fe98b868e6428a1dd29b56b3259
Matthew J Collins <mjcollin@users.noreply.github.com>
2018-08-13 07:22:19 -0400

    Make a larger test

--- sync_hdfs_to_local.sh
+++ sync_hdfs_to_local.sh
@@ -5,8 +5,8 @@
 # No delete flag yet. We will be "archiving" data from HDFS, which is too small for all things of interest,
 # to moose so leave things. Consider a more robust long-term data management plan.
 
-HDFS_PATH="/mnt/guoda-hdfs-nfs/home/"
-LOCAL_PATH="/storage/guoda-hdfs/home/"
+HDFS_PATH="/mnt/guoda-hdfs-nfs/guoda/data/idigbio*"
+LOCAL_PATH="/storage/guoda-hdfs/guoda/data/"
 
 dt=`date --rfc-3339=seconds`
 echo "Starting sync at $dt"
